e4448b3e9ce12249efb81ffcaac03b6a80254f2f
Ayetullah Ünlü <ayetullahbm@gmail.com>
2022-12-09 01:02:03 +0300

    Proje iskeleti ve Core katmanı oluşturuldu

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T: class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);//direk veritabanına sorgu atmamak için IQueryable kullanıldı. tolist vs ile sorgu atılır
+        //productRepository.Where(x => x.Id>5).orderby.tolistasync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);        
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);        
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
---
+++ NLayer.Core/Services/IService.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace NLayer.Core.Services
+{
+    public interface IService<T> where T:class
+    {
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> expression);//direk veritabanına sorgu atmamak için IQueryable kullanıldı. tolist vs ile sorgu atılır
+        //productRepository.Where(x => x.Id>5).orderby.tolistasync();
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task RemoveAsync(T entity);
+        Task RemoveRangeAsync(IEnumerable<T> entities);
+    }
+}
